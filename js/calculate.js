431303877e0770b86fca8ed13de597a2428a04e0
Mahfuj-HSTU <rmahfujur16@gmail.com>
2022-08-22 20:58:38 +0600

    final commit

--- js/calculate.js
+++ js/calculate.js
@@ -1,9 +1,15 @@
 let count = 1;
-// function getElementValuById(name) {
-//   const elemente = document.getElementById(name);
-//   const Name = elemente;
-//   return Name;
-// }
+function getInputFieldValuById(elementId) {
+  const elementeValue = document.getElementById(elementId);
+  const value = parseInt(elementeValue.value);
+  return value;
+}
+
+function setElementFieldValueById(elemenId, value) {
+  const getElementField = document.getElementById(elemenId);
+  getElementField.innerText = value;
+}
+
 function setElementValueById(element, value) {
   const nameList = document.getElementById(element);
   const p = document.createElement("p");
@@ -14,20 +20,12 @@ function setElementValueById(element, value) {
   return count;
 }
 
-// document.getElementById("btn-1").addEventListener("click", function () {
-//   const name = getElementValuById("name-1");
-//   name.disabled = true;
-//   // console.log(name);
-//   setElementValueById("selected", name);
-// });
-
 const elements = document.querySelectorAll(".btn-select");
-
 for (const names of elements) {
   let parentElements = names.parentNode;
   const name = parentElements.firstElementChild.innerText;
-  names.addEventListener("click", function (value) {
-    const selectButton = value.target.innerText;
+  names.addEventListener("click", function (event) {
+    names.setAttribute("disabled", true);
     if (count === 6) {
       alert("You can not select more than 5 players");
       return;
@@ -36,15 +34,27 @@ for (const names of elements) {
   });
 }
 
+// const elements = document.getElementsByClassName("btn-select");
+// for (let i = 0; i < 6; i++) {
+//   let parentElements = elements[i].parentNode;
+//   const name = parentElements.firstElementChild.innerText;
+//   elements[i].addEventListener("click", function () {
+//     elements[i].setAttribute("disabled", "");
+//     if (count === 6) {
+//       alert("You can not select more than 5 players");
+//       return;
+//     }
+//     setElementValueById("selected", name);
+//   });
+// }
+
 // calculation part
 document.getElementById("calculate").addEventListener("click", function () {
-  const eachPlayerPriceElement = document.getElementById("player-price");
-  const eachPlayerPrice = parseInt(eachPlayerPriceElement.value);
+  const eachPlayerPrice = getInputFieldValuById("player-price");
   const selectedPlayers = document.getElementById("selected");
   const players = selectedPlayers.children.length - 1;
   const playersPrieces = eachPlayerPrice * players;
-  const playersPricesElement = document.getElementById("players-price");
-  playersPricesElement.innerText = playersPrieces;
+  setElementFieldValueById("players-price", playersPrieces);
 });
 
 // total calculation
@@ -54,13 +64,8 @@ document
   .addEventListener("click", function () {
     const playersPricesElement = document.getElementById("players-price");
     const playersPrices = parseInt(playersPricesElement.innerText);
-    const managerPriceElement = document.getElementById("manager-price");
-    const managerPrice = parseInt(managerPriceElement.value);
-    const coachPriceElement = document.getElementById("coach-price");
-    const coachPrice = parseInt(coachPriceElement.value);
+    const managerPrice = getInputFieldValuById("manager-price");
+    const coachPrice = getInputFieldValuById("coach-price");
     const totalPrice = playersPrices + managerPrice + coachPrice;
-
-    const totalPriceElement = document.getElementById("total-price");
-    totalPriceElement.innerText = totalPrice;
-    // console.log(totalPrice);
+    setElementFieldValueById("total-price", totalPrice);
   });
